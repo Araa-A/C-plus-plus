ab57ebc2eb13010cc5872dc47959dce155860830
Araa A. Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2025-01-24 11:19:34 +0800

    Create hello-world.cpp

---
+++ hello/hello-world.cpp
@@ -0,0 +1,10 @@
+# include <iostream> // header file that includes functions for basic input and output opperations
+
+int main(){ //  C++ standard requires main() to return int
+	std::cout << "I like macaroni" << std::endl;
+	std::cout << "It's so sugoi"<< std::endl;
+	return 0;
+}
+
+// :: is scope resolution opperator
+// << is the standard character output, which is also known as the left shift opperator
