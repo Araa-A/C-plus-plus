dcd5c1eab4607dc527b527f5e7f8288dd722e1bc
Araa A. Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2025-01-24 11:20:03 +0800

    Create hello-variables.cpp

---
+++ hello/hello-variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+	// declaration int x; 
+	// assignment x = 5; 
+	int x = 5;
+	int y = 6;
+	int sum = x + y;
+
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+	std::cout << sum << std::endl;
+
+	return 0;
+}
